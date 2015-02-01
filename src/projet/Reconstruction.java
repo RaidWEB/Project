d5d1a0c186947f616cea5b5cfe0311cfe8247d8e
RaidWEB <RaidWEB@users.noreply.github.com>
2015-02-01 13:49:08 +0100

    Update Reconstruction.java

--- src/projet/Reconstruction.java
+++ src/projet/Reconstruction.java
@@ -8,6 +8,9 @@ public class Reconstruction {
  /**
  *
  * @author Munoz and Porret
+ * * Cette oeuvre, création, site ou texte est sous licence Creative Commons Attribution -
+* Pas d’Utilisation Commerciale 4.0 International.
+* Pour accéder à une copie de cette licence, merci de vous rendre à l'adresse suivante http://creativecommons.org/licenses/by-nc/4.0/ ou envoyez un courrier à Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  */
     public static void reconstruire (String src, String src1, String src2, String dst,JButton rebuildButton,Interface_principale interface_principale,ProgressBar_resynchro progressBar_resynchro) {
         final int MAX =3;
