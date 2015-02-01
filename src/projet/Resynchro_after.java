52f8a629d98e888176a5443dd430b5bdc1646e07
RaidWEB <RaidWEB@users.noreply.github.com>
2015-02-01 13:51:22 +0100

    Update Resynchro_after.java

--- src/projet/Resynchro_after.java
+++ src/projet/Resynchro_after.java
@@ -17,6 +17,9 @@ import java.util.logging.Logger;
 /**
  *
  * @author Munoz and Porret
+ * * Cette oeuvre, création, site ou texte est sous licence Creative Commons Attribution -
+* Pas d’Utilisation Commerciale 4.0 International.
+* Pour accéder à une copie de cette licence, merci de vous rendre à l'adresse suivante http://creativecommons.org/licenses/by-nc/4.0/ ou envoyez un courrier à Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  */
 public class Resynchro_after implements Runnable{
     
